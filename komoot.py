fcc3a0139e336f1e870eca4ea3e218f18ad1bbec
Tom Flanagan <tom@zkpq.ca>
2019-01-12 19:15:48 -0800

    Fix json parsing

--- komoot.py
+++ komoot.py
@@ -50,17 +50,41 @@ class KomootExport(object):
 
     def get_tours(self, user_id):
         tours_html = self.get_tours_html(user_id)
-        tours = self.parse_tours(tours_html)['tours']
-        log.debug('Loaded %d tours for user_id %s', len(tours), user_id)
+        data = self.parse_tours(tours_html)
+        tours = data['user']['tours']['items']
+        log.info('Loaded %d tours for user_id %s', len(tours), user_id)
         return tours
+
+    def get_tours_loggedin(self, user_id):
+        r = self.session.get(
+            'https://api.komoot.de/v007/users/{}/tours/'.format(user_id))
+        r.raise_for_status()
+        data = r.json()
+        tours = data['_embedded']['tours']['items']
+        log.info('Loaded %d tours for user_id %s', len(tours), user_id)
+        raise NotImplimentedError
 
     @staticmethod
     def parse_tours(html):
+        # TODO this only loads the first page of tours (24)
         start_index = html.index('kmtBoot.setProps(')
         end_index = html.index(');\n    </script>', start_index);
         data = html[start_index + 18 : end_index - 1]
         data = data.encode('utf-8').decode('unicode_escape')
         data = json.loads(data)
+        def fix(j):
+            if isinstance(j, dict):
+                e = j.pop('_embedded', None)
+                d = {k:fix(v) for k, v in j.items()}
+                if e is not None:
+                    d.update(fix(e))
+                return d
+            elif isinstance(j, list):
+                return [fix(v) for v in j]
+            else:
+                return j
+        data = fix(data)
+        log.debug(json.dumps(data, sort_keys=True, indent=2))
         return data
 
     def get_tour_gpx(self, tour_id):
@@ -77,7 +101,9 @@ class KomootExport(object):
             for k,v in (p.split('=') for p in params)}
         filename = urllib.parse.unquote(params['filename'])
         filename = filename.translate({ord(i):'_' for i in r' ,<.>/?;:\'"[{]}\|=+'})
-        log.debug('Got tour %s with filename %s', tour_id, filename)
+        if filename.endswith('_gpx'):
+            filename = filename[:-4] + '.gpx'
+        log.debug('Downloaded tour %s to %s', tour_id, filename)
         return filename, r.text
 
     def download_all_tours(self, user_id=None):
@@ -95,15 +121,15 @@ class KomootExport(object):
             tour_id = tour['id']
             exists = already_downloaded(tour_id)
             if exists:
-                log.info('{} is already synced as {}'.format(tour_id, exists))
+                log.info('Tour {} is already synced as {}'.format(tour_id, exists))
             else:
                 filename, gpx = self.get_tour_gpx(tour_id)
                 open(filename, 'w', encoding='utf-8').write(gpx)
-                log.info('{} saved as {}'.format(tour_id, filename))
+                log.info('Tour {} saved as {}'.format(tour_id, filename))
 
 
 def export():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--user-name',
         help='User name (email) to log in as. Not required to export public tours.',
